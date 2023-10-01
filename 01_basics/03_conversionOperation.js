9627e422077740adee38202bf94d0b993b13bde7
Ravi Teja <125200085+Raviteja0405@users.noreply.github.com>
2023-10-01 10:37:02 +0000

    Added conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = 83;
+let score = "83abc"; //null => 0, undefined =>NaN;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33"=>33
+// "33av" => NaN
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// "" => false
+// "name" => true
+
+// String ..
