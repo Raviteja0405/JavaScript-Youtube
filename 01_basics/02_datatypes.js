348f999107fd819691d617f21777049502c0833d
Ravi Teja <125200085+Raviteja0405@users.noreply.github.com>
2023-10-01 10:25:30 +0000

    added Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all Js as newer version.
+
+// alert(2+2);//we are using nodejs, not browser.
+
+console.log(3+3);
+console.log("Mowa");
+
+let naame = "mowa";
+let age = 10;
+let isLoggedIn = true;
+
+//number
+// BigInt
+// string =>
+// Boolean
+// null => stand alone value 
+// undefined
+// Symbol =>unique
+
+
+// Object
+
+// typeof();
+console.log(typeof(null)); //important
+console.log(typeof(undefined));
