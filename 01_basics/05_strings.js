8b08164070d364d22f2470a75b93ef1b5a49899e
Ravi Teja <125200085+Raviteja0405@users.noreply.github.com>
2023-10-01 12:53:27 +0000

    edited strings.js

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -26,10 +26,10 @@ const newStringOne = "   hitesh    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://hitesh.com/hitesh%20choudhary";
 
-console.log(url.replace('%20', '-'))
+console.log(url.replace('%20', '-'));
 
-console.log(url.includes('sundar'))
+console.log(url.includes('sundar'));
 
 console.log(gameName.split('-'));
