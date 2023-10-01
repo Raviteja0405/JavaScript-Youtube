211995feed3cdcb9a5d67dd747ae4e21027b0bd7
Ravi Teja <125200085+Raviteja0405@users.noreply.github.com>
2023-10-01 10:14:18 +0000

    Learned Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1334
+let accountEmailId = "mowa@google.com"
+var accountPassword = "mowabro"
+accountCity = "jaipur"
+let accountState;
+console.log(accountId);
+
+console.table([accountId, accountEmailId, accountPassword, accountState]);
+
+/*
+NOTE:
+Preferred not to use var.
+*/
